c846fb83391d1496b2ce6c09e29fdf61c6721903
zhengjianliu <comezheng@gmail.com>
2020-10-17 21:53:05 -0400

    Auto play while scroll

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q="
-const API = "&key=AIzaSyDhk-6B4fZIARhe1qZdr3MiSs-QIPQN2Fg"
+const API = "&key=AIzaSyBLLg76TCqpdFTUARyTwYrgRhnxnesChRs"
 document.addEventListener('DOMContentLoaded', () => {
 
   const renderVideos = (videos) => {
@@ -12,9 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const renderVideo = (video) => {
       const videoId = video.id.videoId
       const videosBox = document.querySelector('#fullPage')
-      const videoDiv = document.createElement('div')
+      const videoDiv = document.createElement('section')
       videoDiv.setAttribute('class','section')
-      videoDiv.setAttribute('id','video')
       videoDiv.innerHTML = `
       <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}?autoplay=0""
         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
@@ -42,7 +41,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
+  const validation = (elements) =>{
+    for (element of elements.children ){
+      var rect = element.getBoundingClientRect()
+      var elemTop = rect.top;
+      var elemBottom = rect.bottom;
+      var isVisible = (elemTop >=0) && (elemBottom <= window.innerHeight)
+      if (isVisible){
+        return element
+      }
+    }
+  }
+
+  const body = document.querySelector('#fullPage')
+
+  body.addEventListener('scroll', ()=>{
+    const currentVideo = validation(body)
+    if (currentVideo!= undefined){
+      const frame = currentVideo.children[0]
+      const pausedVideo = frame.src.slice(0,-1)
+      const playVideo = pausedVideo + "1"
+      frame.src = playVideo
+      console.log(frame.src)
+    }
+  })
+
   /* ----------- */
   renderData()
   searchHandler()
+
 })
