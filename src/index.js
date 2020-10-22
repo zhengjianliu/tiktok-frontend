912b479348f9e4163bb236a085e828d183887c70
zhengjianliu <comezheng@gmail.com>
2020-10-21 21:37:13 -0400

    imporved UX

--- src/index.js
+++ src/index.js
@@ -12,41 +12,41 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const renderVideo = (video) => {
-      const videoId = video.id.videoId
-      if(videoId !== undefined){
-        const videosBox = document.querySelector('#fullPage')
-        const videoDiv = document.createElement('section')
-        videoDiv.setAttribute('class','section')
-        videoDiv.setAttribute('data-video-id',`${videoId}`)
-        videoDiv.innerHTML=`
+    const videoId = video.id.videoId
+    if (videoId !== undefined) {
+      const videosBox = document.querySelector('#fullPage')
+      const videoDiv = document.createElement('section')
+      videoDiv.setAttribute('class', 'section')
+      videoDiv.setAttribute('data-video-id', `${videoId}`)
+      videoDiv.innerHTML = `
           <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}?autoplay=0""
           frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
           gyroscope; picture-in-picture" allowfullscreen></iframe>
         `
-        videosBox.append(videoDiv)
-      }
+      videosBox.append(videoDiv)
+    }
   }
 
-  const renderData = (searchinput,token='&pageToken='+nextpage) => {
-    if(token == "&pageToken="){
+  const renderData = (searchinput, token = '&pageToken=' + nextpage) => {
+    if (token == "&pageToken=") {
       token = ""
     }
-    if(searchinput == undefined){
+    if (searchinput == undefined) {
       searchinput = "music"
     }
     console.log(searchinput)
-    fetch(URL  + searchinput + token + API)
+    fetch(URL + searchinput + token + API)
       .then(resp => resp.json())
       .then(videos => {
         renderVideos(videos.items)
         nextpage = videos.nextPageToken
-        console.log(URL  + searchinput + token + API)
+        console.log(URL + searchinput + token + API)
       })
   }
 
-  const searchHandler = () =>{
+  const searchHandler = () => {
     const searchBar = document.querySelector('#search-bar')
-    searchBar.addEventListener('submit', e=>{
+    searchBar.addEventListener('submit', e => {
       e.preventDefault()
       const form = e.target
       searchinput = form.input.value
@@ -57,13 +57,13 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  const watching = (elements) =>{
-    for (element of elements.children ){
+  const watching = (elements) => {
+    for (element of elements.children) {
       var rect = element.getBoundingClientRect()
       var elemTop = rect.top;
       var elemBottom = rect.bottom;
-      var isVisible = (elemTop >=0) && (elemBottom <= window.innerHeight)
-      if (isVisible){
+      var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight)
+      if (isVisible) {
         return element
       }
     }
@@ -71,55 +71,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const body = document.querySelector('#fullPage')
 
-  body.addEventListener('scroll', ()=>{
+  body.addEventListener('scroll', () => {
     const currentVideo = watching(body)
-    if (currentVideo!= undefined){
+    if (currentVideo != undefined) {
       const frame = currentVideo.children[0]
-      const pausedVideo = frame.src.slice(0,-1)
+      const pausedVideo = frame.src.slice(0, -1)
       const playVideo = pausedVideo + "1"
       frame.src = playVideo
       const lastVideo = currentVideo.previousSibling
       const lastFrame = lastVideo.children[0]
-      const playingVideo = lastFrame.src.slice(0,-1)
+      const playingVideo = lastFrame.src.slice(0, -1)
       const stopedVideo = playingVideo + "0"
       lastFrame.src = stopedVideo
       // console.log(currentVideo)    /*show current video object <section>*/
       const nextVideo = currentVideo.nextSibling
-      if(nextVideo === null){
+      if (nextVideo === null) {
         console.log('no more') /*when last video is shown, console log no more*/
         renderData(searchinput)
-      }else{
+      } else {
         const nextFrame = nextVideo.children[0]
-        const nextplayingVideo = nextFrame.src.slice(0,-1)
+        const nextplayingVideo = nextFrame.src.slice(0, -1)
         const nextstopedVideo = nextplayingVideo + "0"
         nextFrame.src = nextstopedVideo
       }
     }
+
+    const sidepanel = document.querySelector('.sidepanel')
+    const closebutton = document.querySelector('#closebutton')
+    const favorsidepanel = document.querySelector('.favorsidepanel')
+    const favorclosebutton = document.querySelector('#favorclosebutton')
+    sidepanel.style.width = '0';
+    closebutton.style.display = "none";
+    closebutton.style.right = "0";
+    favorsidepanel.style.width = '0';
+    favorclosebutton.style.display = "none";
+    favorclosebutton.style.right = "0";
+
+
   })
 
-  const clickHandler = () =>{
-    document.addEventListener('click', e=>{
+  const clickHandler = () => {
+    document.addEventListener('click', e => {
       const clickTarget = e.target
-      const icon  = clickTarget.parentElement
+      const icon = clickTarget.parentElement
       const sidepanel = document.querySelector('.sidepanel')
       const closebutton = document.querySelector('#closebutton')
       const favorsidepanel = document.querySelector('.favorsidepanel')
       const favorclosebutton = document.querySelector('#favorclosebutton')
 
-      if(icon.id == 'account' || icon.parentElement.id == 'account'){
+      if (icon.id == 'account' || icon.parentElement.id == 'account') {
         sidepanel.style.width = '300px';
         closebutton.style.right = '320px';
         closebutton.style.display = 'block';
-      }else if(icon.id == 'heart' || icon.parentElement.id == 'heart'){
-          clickTarget.src = 'src/redheart.png'
-          const currentVideo = watching(body)
-          const videoId = currentVideo.dataset.videoId
-          console.log(videoId) /* CLick like to get the videoId*/
-      }else if(icon.id == 'favorite' || icon.parentElement.id == 'favorite'){
+      } else if (icon.id == 'heart' || icon.parentElement.id == 'heart') {
+        clickTarget.src = 'src/redheart.png'
+        const currentVideo = watching(body)
+        const videoId = currentVideo.dataset.videoId
+        console.log(videoId) /* CLick like to get the videoId*/
+      } else if (icon.id == 'favorite' || icon.parentElement.id == 'favorite') {
         favorsidepanel.style.width = '300px';
         favorclosebutton.style.right = '320px';
         favorclosebutton.style.display = 'block';
-      }else if(clickTarget.id == 'closebutton' || clickTarget.id == 'favorclosebutton' || clickTarget.id == 'logout'){
+      } else if (clickTarget.id == 'closebutton' || clickTarget.id == 'favorclosebutton' || clickTarget.id == 'logout') {
         sidepanel.style.width = '0';
         closebutton.style.display = "none";
         closebutton.style.right = "0";
@@ -129,25 +142,25 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
 
-    document.addEventListener('click', e=>{
+    document.addEventListener('click', e => {
       const loginform = document.querySelector('.loginform')
       const signupform = document.querySelector('.signupform')
       const button = e.target
-      if(button.id == "loginbutton"){
-        loginform.style.display ="flex";
-      }else if (button.id == "signup"){
+      if (button.id == "loginbutton") {
+        loginform.style.display = "flex";
+      } else if (button.id == "signup") {
         console.log('signup')
-        signupform.style.display ="flex";
-        loginform.style.display ="none";
-      }else if (button.id == "closeform"){
-        loginform.style.display ="none";
-        signupform.style.display ="none";
+        signupform.style.display = "flex";
+        loginform.style.display = "none";
+      } else if (button.id == "closeform") {
+        loginform.style.display = "none";
+        signupform.style.display = "none";
       }
     })
   }
 
   /* ----------- */
-  // renderData()
+  renderData()
   searchHandler()
   clickHandler()
 })
--- src/login.js
+++ src/login.js
@@ -3,59 +3,53 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginform = document.querySelector('#login-form')
   const signupform = document.querySelector('#signup-form')
   const stickybuttons = document.querySelector('.sticky-button')
-
   let userId = ''
 
   const loginHandler = () => {
-
     loginform.addEventListener('submit', e => {
-
-        e.preventDefault()
-        const form = e.target
-        let username = form.username.value
-        let password = form.password.value
-        form.lastChild.remove()
-        validation(username, password)
-        form.reset()
-        const errors = document.createElement('p')
-        errors.innerHTML =`Invalid Username or Password`
-        errors.style.color = 'red';
-        form.append(errors)
-
-
+      e.preventDefault()
+      const form = e.target
+      let username = form.username.value
+      let password = form.password.value
+      form.lastChild.remove()
+      validation(username, password)
+      form.reset()
+      const errors = document.createElement('p')
+      errors.innerHTML = `Invalid Username or Password`
+      errors.style.color = 'red';
+      form.append(errors)
     })
   }
 
-  const validation = (username, password) =>{
+  const validation = (username, password) => {
     fetch(userUrl)
       .then(resp => resp.json())
       .then(users => {
-          for (const user of users) {
-            if (username == user.userName && password == user.password) {
-              userId = user.id
-              loginform.style.display ="none"
-              if(userId!= ''){
-                  const stickybutton = stickybuttons.children
-                  const account = stickybutton[0].style.display = 'block';
-                  const heart = stickybutton[1].style.display = 'block';
-                  const favor = stickybutton[2].style.display = 'block';
-                  const login = stickybutton[3].style.display = 'none';
-                  const name = document.querySelector('#username')
-                  name.textContent = user.fullName
-              }
+        for (const user of users) {
+          if (username == user.userName && password == user.password) {
+            userId = user.id
+            loginform.style.display = "none"
+            if (userId != '') {
+              const stickybutton = stickybuttons.children
+              const account = stickybutton[0].style.display = 'block';
+              const heart = stickybutton[1].style.display = 'block';
+              const favor = stickybutton[2].style.display = 'block';
+              const login = stickybutton[3].style.display = 'none';
+              const name = document.querySelector('#username')
+              name.textContent = user.fullName
             }
           }
         }
-      )
+      })
   }
 
-  const clickHandler = () =>{
-    document.addEventListener('click', e=>{
+  const clickHandler = () => {
+    document.addEventListener('click', e => {
       const button = e.target
-      if(button.id == "loginbutton"){
+      if (button.id == "loginbutton") {
         loginform.style.display = "block"
-        loginform.lastChild.innerHTML= ''
-      }else if(button.id == 'logout'){
+        loginform.lastChild.innerHTML = ''
+      } else if (button.id == 'logout') {
         userId = ''
         const stickybutton = stickybuttons.children
         const account = stickybutton[0].style.display = 'none';
@@ -67,9 +61,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-/*----------*/
-loginHandler()
-clickHandler()
+  /*----------*/
+  loginHandler()
+  clickHandler()
 
 
 })
